8c43cc3d0ee09f1be8df11f18a18072187bb8f76
Imran Hossen Bappy <61227100+imranBappy@users.noreply.github.com>
2022-07-27 00:34:59 +0600

    insert function and printList done

---
+++ hello.c
@@ -0,0 +1,48 @@
+// A simple C program for traversal of a linked list
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node
+{
+    int data;
+    struct Node *next;
+} Node;
+Node *head = NULL;
+void insert(int data)
+{
+
+    Node *newNode = malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    if (!head)
+    {
+        head = newNode;
+        return;
+    }
+    Node *node = head;
+    while (node->next)
+    {
+        node = node->next;
+    }
+    node->next = newNode;
+}
+
+void printList(struct Node *n)
+{
+    while (n != NULL)
+    {
+        printf(" %d ", n->data);
+        n = n->next;
+    }
+}
+
+int main()
+{
+    insert(1);
+    insert(2);
+    insert(3);
+
+    printList(head);
+
+    return 0;
+}
--- linked-list.c
+++ linked-list.c
@@ -1,20 +1,52 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 typedef struct Node
 {
     int data;
     struct Node *next;
 } Node;
 
-Node *head;
+Node *head = NULL;
 
-void createNode(Node *head)
+void insert(int data)
 {
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    if (head == NULL)
+    {
+        head = newNode;
+        return;
+    }
+    Node *node = head;
+    while (node->next)
+    {
+        node = node->next;
+    }
+    node->next = newNode;
+}
+
+void printList()
+{
+    Node *node = head;
+    if (!head)
+    {
+        printf("List is empty\n");
+        return;
+    }
+    while (node)
+    {
+        printf("%d, ", node->data);
+        node = node->next;
+    }
 }
 int main()
 {
-
-    printf("Hello World!");
-
+    insert(130);
+    insert(100);
+    insert(200);
+    insert(300);
+    insert(1000);
+    printList();
     return 0;
 }
