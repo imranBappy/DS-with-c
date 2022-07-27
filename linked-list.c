bfa543b5de8b14ce3962078b2efcd15cbc75fe75
Imran Hossen Bappy <61227100+imranBappy@users.noreply.github.com>
2022-07-27 10:58:31 +0600

    delete function done

--- linked-list.c
+++ linked-list.c
@@ -25,7 +25,42 @@ void insert(int data)
     }
     node->next = newNode;
 }
-
+Node *find(int data)
+{
+    Node *node = head;
+    while (node)
+    {
+        if (node->data == data)
+        {
+            return node;
+        }
+        node = node->next;
+    }
+    return NULL;
+}
+void delete (int data)
+{
+    Node *node = head;
+    Node *prev = NULL;
+    while (node)
+    {
+        if (node->data == data)
+        {
+            if (!prev)
+            {
+                head = node->next;
+                free(node);
+                return;
+            }
+            prev->next = node->next;
+            free(node);
+            return;
+        }
+        prev = node;
+        node = node->next;
+    }
+    printf("Data Not Found\n");
+}
 void printList()
 {
     Node *node = head;
@@ -47,6 +82,17 @@ int main()
     insert(200);
     insert(300);
     insert(1000);
+    delete (1000);
+
     printList();
+    Node *node = find(1000);
+    if (node)
+    {
+        printf("\nNode found with data %d\n", node->data);
+    }
+    else
+    {
+        printf("\nNode not found\n");
+    }
     return 0;
 }
