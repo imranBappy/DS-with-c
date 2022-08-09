f5e418d21a1a1f4187a1850ad84101e732ae98a9
Imran Hossen Bappy <61227100+imranBappy@users.noreply.github.com>
2022-08-09 15:15:06 +0600

    counting sort done

--- counting.c
+++ counting.c
@@ -7,10 +7,8 @@ int *counting_sort(int arr[], int n, int max)
     int *output = (int *)malloc(n * sizeof(int));
     for (int i = 0; i < max + 1; i++)
         count[i] = 0;
-
     for (int i = 0; i < n; i++)
         count[arr[i]]++;
-
     sum[0] = count[0];
     for (int i = 1; i < max + 1; i++)
         sum[i] = count[i] + sum[i - 1];
